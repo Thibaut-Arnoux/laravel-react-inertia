34acaa7e75df61d43c090b507667077443683d74
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-01-02 12:27:14 +0100

    fix: remove tailwindcss/forms which interfere with daysi ui toggle button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-import forms from '@tailwindcss/forms';
 import daisyui from 'daisyui';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
@@ -19,7 +18,7 @@ export default {
         },
     },
 
-    plugins: [forms, daisyui],
+    plugins: [daisyui],
     daisyui: {
         themes: ['light', 'dark'],
     },
